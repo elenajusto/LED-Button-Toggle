41ade1e7c51531516328f328df49521e5e16798f
Elena <elenajusto@users.noreply.github.com>
2024-01-08 10:24:35 +1100

    Added comments to skeleton of code

--- src/main.c
+++ src/main.c
@@ -1,6 +1,9 @@
 // Header Files
-#include <avr/io.h>
+#include <avr/io.h>         // Pin definitions for AVR
+#include <util/delay.h>     // Delay functions for AVR
 
+// Constants
+#define F_CPU 16000000UL    // Arduino board clock frequency: 16 Mhz
 
 // Main Program
 void main(void) {

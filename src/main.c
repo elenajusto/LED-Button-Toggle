c93bf3433c3153a036e954359de4718b4d08333b
Elena <elenajusto@users.noreply.github.com>
2024-01-09 07:33:37 +1100

    Implemented logic to check button input. Reorganised bitmask variable placement in code. Created build folder. Main.hex was successfully uploaded to the AVR Atmega328 on the Arduino board using AVRDUDE and the button/light functions well :)

--- src/main.c
+++ src/main.c
@@ -10,24 +10,24 @@ int main(void) {
 
     // Setting the value of DDRD
     // Default value of DDRD is 0000 0000
-    int valueToSetDDRD = 0b10000000;   // Bitmask for DDRD to set D7 to 1
-    DDRD = DDRD | valueToSetDDRD;      // Carries out OR between 0000 0000 and 1000 0000              
+    int valueToSetDDRD = 0b10000000;         // Bitmask for DDRD to set D7 to 1
+    DDRD = DDRD | valueToSetDDRD;            // Carries out OR between 0000 0000 and 1000 0000              
+
+    // Bitmasks
+    int compareOnBitmask = 0b01000000;       // Bitmask where PIN6 is reading HIGH (1)
+    int valueHighBitmask = 0b10000000;       // Bitmask for PORTD to set D7 to 1
+    int valueLowBitmask = 0b01111111;        // Bitmask for PORTD to set D7 to 0, leave all other states alone
 
     // Main Loop
     while(1) {
-        // If PIN D6 is HIGH
-        
+        // If PIN D6 is reading HIGH then == 0100 0000 is TRUE
+        if ((PIND & compareOnBitmask) == 0b01000000){
             // Set PORT D7 to HIGH
-            // Default value of PORTD is 0000 0000
-            int valueHighBitmask = 0b10000000;          // Bitmask for PORTD to set D7 to 1
             PORTD = PORTD | valueHighBitmask;           // Carries out OR between 0000 0000 and 1000 000
-
-        // Else
-
+        } else {
             // Set PORT PD7 to LOW
-            // Assuming value of PORTD is 1000 0000
-            int valueLowBitmask = 0b01111111;          // Bitmask for PORTD to set D7 to 0, leave all other states alone
             PORTD = PORTD & valueLowBitmask;           // Carries out AND between 1000 0000 and 0000 0000
+        }
     }
     return 0;
 }
